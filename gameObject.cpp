54ce2b92cc61ac2867b72a0531d5c8096899b818
ssp237 <ssp237@cornell.edu>
2019-05-15 23:51:59 -0400

    merge o no

--- gameObject.cpp
+++ gameObject.cpp
@@ -14,7 +14,7 @@ Player::Player(float init_x, float init_y)
 
 void Player::draw(GameBoard &b)
 {
-  b.draw(floor(x), floor(y), ORANGE);
+  b.draw(floor(x), floor(y), GREEN);
 }
 
 void Player::update(float dt)
@@ -26,4 +26,42 @@ void Player::update(float dt)
 ObstacleType Player::getType()
 {
   return PLAYER;
+}
+
+/**
+ * Create a rock with position init_x, init_y and velocity 0
+ */
+Rock::Rock(float init_x, float init_y)
+{
+  x = init_x;
+  y = init_y;
+  dx = 0;
+  dy = 0;
+}
+
+/**
+ * Draw this rock to the game board
+ */
+void Rock::draw(GameBoard &board)
+{
+  board.draw(floor(x), floor(y), RED);
+}
+
+/**
+ * Create a coin with position init_x, init_y and velocity 0
+ */
+Coin::Coin(float init_x, float init_y)
+{
+  x = init_x;
+  y = init_y;
+  dx = 0;
+  dy = 0;
+}
+
+/**
+ * Draw this rock to the game board
+ */
+void Coin::draw(GameBoard &board)
+{
+  board.draw(floor(x), floor(y), ORANGE);
 }
